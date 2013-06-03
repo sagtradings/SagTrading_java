9b097eeb20b414abe763333c46f0b6adb1b361c1
sagtrading <sagtradings@gmail.com>
2013-06-03 18:20:11 -0400

    refactoring to eliminate hardcoding from quotation requests

--- src/nativeinterfaces/DefaultNativeInterface.java
+++ src/nativeinterfaces/DefaultNativeInterface.java
@@ -8,7 +8,7 @@ public class DefaultNativeInterface{
 	
 	public native void  subscribeListener(DefaultCTPListener obj);
     public native void unSubscribeListener(DefaultCTPListener obj);
-	public native void sendLoginMessage(String[] packet);
+    public native void sendLoginMessage(String brokerId, String password, String investorId);
 	public native void sendOrderInsertMessage(String[] packet);
 	public native void sendReturnOrderMessage(String[] packet);
 	public native void sendQuoteRequest(String[] insturments);
