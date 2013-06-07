695f5ac3072d4ae385cd1e47f278d9b8492d9eaa
sagtrading <sagtradings@gmail.com>
2013-06-07 14:01:44 -0400

    events for Trading

---
+++ src/listeners/StopLossListener.java
@@ -0,0 +1,46 @@
+package listeners;
+
+import nativeinterfaces.DefaultNativeInterface;
+import nativeinterfaces.TradingNativeInterface;
+import orderrepository.OrderBucket;
+import orderrepository.OrderRepository;
+
+import bo.OrderActionRequest;
+import bo.TradeRequest;
+
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+public class StopLossListener implements UpdateListener {
+
+	@Override
+	public void update(EventBean[] arg0, EventBean[] arg1) {
+		for(int i = 0, n = arg0.length; i < n; i++){
+			String instrument = (String) arg0[i].get("instrumentID");
+			OrderBucket bucket = OrderRepository.getInstance().getOrderBucket(instrument);
+			bucket.setOrderState(OrderBucket.orderStates.STOP_LOSS_COMPLETED);
+			
+			TradeRequest stopLoss = bucket.getStopLossRequest();
+			OrderActionRequest cancelRequest = new OrderActionRequest();
+			cancelRequest.setActionFlag("0");
+			cancelRequest.setBrokerID("1013");
+			cancelRequest.setExchangeID("");
+			cancelRequest.setFrontID(0);
+			cancelRequest.setInvestorID("00000008");
+			cancelRequest.setInstrumentID(instrument);
+			cancelRequest.setLimitPrice(0);
+			cancelRequest.setOrderActionRef(0);
+			cancelRequest.setOrderRef(stopLoss.getOrderRef());
+			cancelRequest.setOrderSysID("");
+			cancelRequest.setRequestID(0);
+			cancelRequest.setSessionID(0);
+			cancelRequest.setUserID("1013");
+			
+			//new TradingNativeInterface().sendTradeRequest("1013", "123321", "00000008", bucket.getStopLossRequest());
+			new TradingNativeInterface().sendOrderAction("1013", "123321", "00000008", cancelRequest);
+			new DefaultNativeInterface().sendUnsubscribeQuoteRequest(new String[] {instrument});
+		}
+
+	}
+
+}
---
+++ src/listeners/TradeMarketDataListener.java
@@ -0,0 +1,79 @@
+package listeners;
+
+import com.espertech.esper.client.EPRuntime;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
+
+import bo.ErrorResult;
+import bo.LoginResponse;
+import bo.MarketDataResponse;
+import bo.OrderActionRequest;
+import bo.OrderInsertResponse;
+import bo.SettlementResponse;
+import bo.TradeDataResponse;
+
+public class TradeMarketDataListener extends DefaultCTPListener {
+
+	@Override
+	public void onRspUserLogin(LoginResponse loginResponse) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onFrontConnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onFrontDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRspOrderInsert() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRtnOrder(OrderInsertResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRspError(ErrorResult errorRslt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRtnDepthMarketData(MarketDataResponse response) {
+		EPServiceProvider epService = EPServiceProviderManager.getDefaultProvider();
+		EPRuntime runtime = epService.getEPRuntime();
+		runtime.sendEvent(response);
+
+	}
+
+	@Override
+	public void onRtnTradingData(TradeDataResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSettlementResponse(SettlementResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onOrderActionResponse(OrderActionRequest initiatingAction) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
