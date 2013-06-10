050f45b80551669db2d2cb62b5883a416eb59558
sagtrading <sagtradings@gmail.com>
2013-06-10 13:00:24 -0400

    renaming of 'Default' to 'MarketData'

--- src/nativeinterfaces/MarketDataNativeInterface.java
+++ src/nativeinterfaces/MarketDataNativeInterface.java
@@ -3,14 +3,12 @@ package nativeinterfaces;
 import listeners.DefaultCTPListener;
 import listeners.ICTPListener;
 
-public class DefaultNativeInterface{
+public class MarketDataNativeInterface{
 
 	
 	public native void  subscribeListener(DefaultCTPListener obj);
     public native void unSubscribeListener(DefaultCTPListener obj);
     public native void sendLoginMessage(String brokerId, String password, String investorId);
-	public native void sendOrderInsertMessage(String[] packet);
-	public native void sendReturnOrderMessage(String[] packet);
 	public native void sendQuoteRequest(String[] insturments);
 	public native void sendUnsubscribeQuoteRequest(String[] insturments);
 
