d660d38f478fdc8c48b65a5761fec92d8e7f0ad1
sagtrading <sagtradings@gmail.com>
2013-06-11 10:53:25 -0400

    Custom Bar data manager

--- src/listeners/NonEsperMarketDataListener.java
+++ src/listeners/NonEsperMarketDataListener.java
@@ -3,15 +3,20 @@ package listeners;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import bardatamanager.EntryNotInitializedException;
 import bo.BarData;
 import bo.MarketDataResponse;
 
 public final class NonEsperMarketDataListener extends DefaultCTPListener {
-
+	private Map<String, Long> startTimes = new HashMap<String, Long>(10);
 	private SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHH:mm:ss");
-	private long startTime = System.currentTimeMillis();
+
+	
+	
+	
 	public void onRtnDepthMarketData(MarketDataResponse response) {
 		double deltaAskPrice1 = (response.getLastPrice() - response.getAskPrice1());
 		double deltaBidPrice1 = response.getLastPrice() - response.getBidPrice1();
@@ -33,11 +38,16 @@ public final class NonEsperMarketDataListener extends DefaultCTPListener {
 		try {
 			BarData compiledData = barManager.sendMarketData(response);
 			if(compiledData != null){
+				Long startTime = startTimes.get(response.getInstrumentId());
+				if(startTime == null){
+					startTime = System.currentTimeMillis();
+					startTimes.put(response.getInstrumentId(), startTime);
+				}
 				long elapsedTime = System.currentTimeMillis() - startTime;
 				System.out.println("got marketData");
-				System.out.println("Open: " + compiledData.getOpen() + " Close: " + compiledData.getClose() + " Low: " + compiledData.getLow() + " High: " + compiledData.getHigh() + " UpVolume: " + compiledData.getUpVolume() + " DownVolume: " + compiledData.getDownVolume());
+				System.out.println("Instrument: " + response.getInstrumentId() + " Open: " + compiledData.getOpen() + " Close: " + compiledData.getClose() + " Low: " + compiledData.getLow() + " High: " + compiledData.getHigh() + " UpVolume: " + compiledData.getUpVolume() + " DownVolume: " + compiledData.getDownVolume());
 				System.out.println("elapsed time: " + elapsedTime);
-				startTime = System.currentTimeMillis();
+				startTimes.put(response.getInstrumentId(), System.currentTimeMillis());
 			}
 		} catch (EntryNotInitializedException e) {
 			// TODO Auto-generated catch block
