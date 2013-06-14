e19cab5847ce14556b1abc5fc79cc26578018f0a
sagtrading <sagtradings@gmail.com>
2013-06-14 16:23:16 -0400

    local testing

--- src/main/Startup.java
+++ src/main/Startup.java
@@ -9,7 +9,7 @@ public class Startup {
 	static{
 		System.loadLibrary("CTPDLL");
 		System.out.println("library loaded");
-		//gussiaas123abcddd
+		//gussiaas
 	}
 	
 	public static void main(String[] args){
