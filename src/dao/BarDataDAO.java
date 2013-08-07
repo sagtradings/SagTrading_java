3021c17c6aa848cfb8abe227de71f345ebef0345
Augie Millares <augie@augiemillares.com>
2013-08-06 22:00:33 -0400

    adding the highest and lowest bardata

--- src/dao/BarDataDAO.java
+++ src/dao/BarDataDAO.java
@@ -1,5 +1,9 @@
 package dao;
 
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -53,6 +57,44 @@ public class BarDataDAO {
         BarData member = (BarData) query.uniqueResult();
         tx.commit();
         return member;
+    }
+
+    public BarData getHighestBarData(String instrumentId, Date startDate, int numberOfPreviousRecord){
+        long startDateTime = startDate.getTime();
+        Session session = SessionUtil.sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query query = session.createQuery("From BarData b where b.timestamp <= " + startDateTime + " and b.instrumentId = " + instrumentId);
+        query.setMaxResults(numberOfPreviousRecord);
+        List<BarData> members = (List<BarData>) query.list();
+        tx.commit();
+
+        return Collections.max(members, new Comparator<BarData>() {
+            @Override
+            public int compare(BarData o1, BarData o2) {
+                if(o1.getHigh() > o2.getHigh()) return 1;
+                if(o1.getHigh() < o2.getHigh()) return -1;
+                return 0;
+            }
+        });
+    }
+
+    public BarData getLowestBarData(String instrumentId, Date startDate, int numberOfPreviousRecord){
+        long startDateTime = startDate.getTime();
+        Session session = SessionUtil.sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query query = session.createQuery("From BarData b where b.timestamp <= " + startDateTime + " and b.instrumentId = " + instrumentId);
+        query.setMaxResults(numberOfPreviousRecord);
+        List<BarData> members = (List<BarData>) query.list();
+        tx.commit();
+
+        return Collections.min(members, new Comparator<BarData>() {
+            @Override
+            public int compare(BarData o1, BarData o2) {
+                if(o1.getHigh() > o2.getHigh()) return 1;
+                if(o1.getHigh() < o2.getHigh()) return -1;
+                return 0;
+            }
+        });
     }
 }
 
