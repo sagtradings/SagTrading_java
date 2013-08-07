be2d137b273d28ba1eef38944fe8dd4d87daabd5
gussiaasArvatoSystems <brian.gussiaas@arvatoSystems.com>
2013-08-06 21:08:35 -0400

    additional test classes

--- src/main/MatLabTradeIntegratorTester.java
+++ src/main/MatLabTradeIntegratorTester.java
@@ -5,11 +5,14 @@ import ctpapiinterface.MatlabTradeIntegrator;
 public class MatLabTradeIntegratorTester {
 	public static void main(String[] args){
 		MatlabTradeIntegrator integrator1 = new MatlabTradeIntegrator(0, "c:\\mddata\\rawdata.csv");
-		MatlabTradeIntegrator integrator2 = new MatlabTradeIntegrator(0, "c:\\mddata\\rawdata.csv");
-		integrator1.requestLogin("", "", "");
-		integrator2.requestLogin("", "", "");
-		integrator1.sendOrder("IF1309", 0, 0, 10000, 10, 6000000);
-		integrator2.sendOrder("IF1309", 0, 0, 10000, 5, 6000000);
+		//MatlabTradeIntegrator integrator2 = new MatlabTradeIntegrator(0, "c:\\mddata\\rawdata.csv");
+		integrator1.requestLogin("1013", "123321", "00000008");
+		integrator1.sendSettlement();
+		
+		
+		//integrator2.requestLogin("", "", "");
+		integrator1.sendOrder("IF1309", 0, 0, 2300, 1, 60000);
+		//integrator2.sendOrder("IF1309", 0, 0, 10000, 5, 6000000);
 		
 	}
 }
